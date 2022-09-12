e2cd7d4efdda899299842d9c807ce24b6ce3d060
Ynabin01 <nabin.01least@gmail.com>
2022-09-12 23:01:47 +0545

    add

--- resources/views/website/joblist.blade.php
+++ resources/views/website/joblist.blade.php
@@ -18,9 +18,9 @@
                             <figure><img src="{{ $job->banner_image }}"></figure>
                             <a href="/jobdetail/{{ $job->nav_name }}">
                                 <div class="job-content">
-                                    <h5>{{ $job->caption }}<span>{{ $job->span }}</span></h5>
-                                    <p><i class="fa fa-industry"></i>{{ $job->short_content }}</p>
-                                    <p><i class="fa fa-map-marker"></i>{{ $job->long_content }}</p>
+                                    <h5>{{ $job->caption }}<span>{{ $job->getJob->total_demand }}</span></h5>
+                                    <p><i class="fa fa-industry"></i>{{ $job->getJob->company_name }}</p>
+                                    <p><i class="fa fa-map-marker"></i>{{ $job->getJob->country }}</p>
                                 </div>
                             </a>
                             <button class="btn-cog">Apply Now</button>
